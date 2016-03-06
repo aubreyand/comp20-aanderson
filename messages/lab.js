b7942799dc9e9a1ee995979d1f3c10edf665691a
Aubrey Anderson <aubrey@bigsur.com>
2016-03-06 01:38:42 -0500

    finished part 1

--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,19 @@
-// Your JavaScript goes here...
+var request;
+function parse() {
+        request = new XMLHttpRequest();
+        var url = "data.json"
+        request.open("GET", url, true);
+
+        request.onreadystatechange = function() {
+                if (request.readyState == 4 && request.status == 200) {
+                        var response = JSON.parse(request.responseText);
+                        var messages = document.getElementById("messages");                 
+                        for (var i = 0; i < response.length; i++) {
+
+                                messages.innerHTML += "<p><span class='user'>" + response[i].username + ": </span>";
+                                messages.innerHTML += response[i].content + "</p><hr>";
+                        }
+                }
+        }
+        request.send();
+}
